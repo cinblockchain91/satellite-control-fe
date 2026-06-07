e371ec02dcadc14b0a88c5ecffc8301cd1d6c281
Matt Hoang <cinblockchain91@gmail.com>
2026-06-07 22:38:08 +0700

    fix: test env vercel

--- packages/shared/config/env.ts
+++ packages/shared/config/env.ts
@@ -1,11 +1,11 @@
 export const env = {
-  apiUrl: process.env.NEXT_PUBLIC_API_URL ?? "http://localhost:4000",
-  wsUrl: process.env.NEXT_PUBLIC_WS_URL ?? "ws://localhost:4000",
-  appEnv: (process.env.NEXT_PUBLIC_APP_ENV ?? "development") as
-    | "development KAKA"
-    | "staging KAKA"
-    | "production KAKA",
-  useMock: process.env.NEXT_PUBLIC_USE_MOCK === "true",
+  apiUrl: process.env.NEXT_PUBLIC_API_URL || "http://localhost:4000",
+  wsUrl: process.env.NEXT_PUBLIC_WS_URL || "ws://localhost:4000",
+  appEnv: (process.env.NEXT_PUBLIC_APP_ENV || "development") as
+    | "development"
+    | "staging"
+    | "production",
+  useMock: process.env.NEXT_PUBLIC_USE_MOCK !== "false",
 } as const;
 
 export type AppEnv = typeof env.appEnv;
