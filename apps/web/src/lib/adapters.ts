0c617033c04c7555b5b54f473f4a6bfeafad65e0
Matt Hoang <cinblockchain91@gmail.com>
2026-06-07 22:44:18 +0700

    fix: remove console.log

--- apps/web/src/lib/adapters.ts
+++ apps/web/src/lib/adapters.ts
@@ -2,11 +2,4 @@ import { authHttpAdapter } from "@satellite-control/infra/http/auth";
 import { authMockAdapter } from "@satellite-control/infra/mock/auth";
 import { env } from "@satellite-control/shared/config";
 
-console.log("[ENV]", {
-  useMock: env.useMock,
-  appEnv: env.appEnv,
-  apiUrl: env.apiUrl,
-  wsUrl: env.wsUrl,
-});
-
 export const authAdapter = env.useMock ? authMockAdapter : authHttpAdapter;
