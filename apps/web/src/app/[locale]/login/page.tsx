ce3df598887285d07b0aae68ad73eebbe6773a3c
Matt Hoang <cinblockchain91@gmail.com>
2026-06-08 23:24:34 +0700

    feat: refactor folder structure for web app

--- apps/web/src/app/[locale]/login/page.tsx
+++ apps/web/src/app/[locale]/login/page.tsx
@@ -1,12 +1,6 @@
 import type { Metadata } from "next";
-import { LoginForm } from "./components/LoginForm";
+import { LoginPage } from "@/views/login/LoginPage";
 
 export const metadata: Metadata = { title: "Login" };
 
-export default function LoginPage() {
-  return (
-    <main className="flex min-h-screen items-center justify-center bg-gray-50">
-      <LoginForm />
-    </main>
-  );
-}
+export default LoginPage;
