bcf3b48aa87e6d68b53244120840ffe7dc445c14
Matt Hoang <cinblockchain91@gmail.com>
2026-06-09 19:18:22 +0700

    fix: vercel preview call localhost

--- packages/shared/config/env.ts
+++ packages/shared/config/env.ts
@@ -1,5 +1,5 @@
 export const env = {
-  apiUrl: process.env.NEXT_PUBLIC_API_URL || "http://localhost:3000",
+  apiUrl: process.env.NEXT_PUBLIC_API_URL || "",
   wsUrl: process.env.NEXT_PUBLIC_WS_URL || "ws://localhost:4000",
   appEnv: (process.env.NEXT_PUBLIC_APP_ENV || "development") as
     | "development"
