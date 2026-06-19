f4807be856d96f167da7e07e338953ac7b3f999e
Matt Hoang <cinblockchain91@gmail.com>
2026-06-19 13:31:57 +0700

    feat: define anomaly detection scenarios and visual rules

---
+++ apps/web/src/widgets/anomaly-area/index.ts
@@ -0,0 +1,7 @@
+export type { AnomalyType, AnomalySeverity, AnomalyDetection } from "./anomaly-detection";
+export {
+  ANOMALY_THRESHOLDS,
+  ANOMALY_VISUAL_RULES,
+  ANOMALY_SEVERITY_COLORS,
+  detectAnomalies,
+} from "./anomaly-detection";
